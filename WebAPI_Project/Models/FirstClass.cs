ca9cb7b940414e3c7ed7d503bea3879d98ecf1ac
Daniel Guinto <danielguinto@yahoo.ca>
2020-09-24 00:33:49 -0400

    Changed HomeController

---
+++ WebAPI_Project/Models/FirstClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Project.Models
+{
+    public class FirstClass
+    {
+        //Sets the value of "a" to -10
+        int a = -10;
+        //Sets the value of "b" to 7
+        int b = 7;
+        //Set the value of "c" to 7
+        int c = 11 + -9 + 5;
+
+        //Sets the value of "d" to "Hello"
+        string d = "Hello";
+
+        //Sets the value of "e" to be "Hello World! It's a nice day"
+        string e = "Hello" + "World!" + "It's a nice day!";
+    }
+}
